8441ae7f60b3f8c0bbad944582080b9feac39b67
Russel Simmons <symstym@mac.com>
2015-11-13 09:42:09 -0800

    Refactor in preparation for upcoming changes

--- main.js
+++ main.js
@@ -42,49 +42,16 @@ function renderPolys(canvas, polys) {
   }
 }
 
-function polysToSVG(image, polys, forPreview) {
-  var framePadFrac = 0.05;
-  var imageMaxDim = Math.max(image.width, image.height);
-  var framePadPx = framePadFrac*imageMaxDim;
-  var withFrameWidthPx = image.width + 2*framePadPx;
-  var withFrameHeightPx = image.height + 2*framePadPx;
-
-  // We give an extra mm of safe area padding beyond what Ponoko says, so we do 6mm instead of 5mm
-  // These safeXXX vars are in mm units
-  var safeAreaWidth = 788;
-  var safeAreaHeight = 382;
-  var safeOffset = 6;
-  var beamWidth = 0.3; // Ponoko says 0.1-0.2mm on either side, so take average
-
-  var polyScale = Math.min(safeAreaWidth/withFrameWidthPx, safeAreaHeight/withFrameHeightPx);
-  var polyOffset = {
-    x: safeOffset + framePadPx*polyScale,
-    y: safeOffset + framePadPx*polyScale,
-  };
-
-  var totalWidth = polyScale*withFrameWidthPx + 2*safeOffset;
-  var totalHeight = polyScale*withFrameHeightPx + 2*safeOffset;
-  var svgWidth = totalWidth + 'mm';
-  var svgHeight = totalHeight + 'mm';
-  var viewBox = '0 0 ' + totalWidth + ' ' + totalHeight;
-
-
-  var frameCutX = safeOffset;
-  var frameCutY = safeOffset;
-  var frameCutWidth = polyScale*withFrameWidthPx;
-  var frameCutHeight = polyScale*withFrameHeightPx;
-
+function polysToSVG(image, polys, jobParams, forPreview) {
   var polyCutStyle;
   var frameCutStyle;
   if (forPreview) {
     // For web preview
-    polyCutStyle = 'stroke="white" stroke-width="' + beamWidth + '" fill="white"'; // simulate slight enlargement due to beam width
+    polyCutStyle = 'stroke="white" stroke-width="' + jobParams.beamWidth + '" fill="white"'; // simulate slight enlargement due to beam width
     frameCutStyle = 'stroke="none" fill="black"';
   } else {
     // For real laser cutting
-    var laserCutColor = 'rgb(0,0,255)';
-    var laserCutStrokeWidth = '0.01';
-    polyCutStyle = 'fill="none" stroke="' + laserCutColor + '" stroke-width="' + laserCutStrokeWidth + '"';
+    polyCutStyle = 'fill="none" stroke="' + jobParams.laserCutColor + '" stroke-width="' + jobParams.laserCutStrokeWidth + '"';
     frameCutStyle = polyCutStyle;
   }
 
@@ -95,8 +62,8 @@ function polysToSVG(image, polys, forPreview) {
     for (var j = 0; j < poly.length; j++) {
       var p = poly[j];
       var tp = {
-        x: p.x*polyScale + polyOffset.x,
-        y: p.y*polyScale + polyOffset.y,
+        x: p.x*jobParams.px2mm + jobParams.insetTranslation.x,
+        y: p.y*jobParams.px2mm + jobParams.insetTranslation.y,
       };
 
       if (j === 0) {
@@ -111,9 +78,9 @@ function polysToSVG(image, polys, forPreview) {
 
   var exportText = '';
   exportText += '<?xml version="1.0" encoding="utf-8"?>\n<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n';
-  exportText += '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="' + svgWidth + '" height="' + svgHeight + '" viewBox="' + viewBox + '">\n';
+  exportText += '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="' + jobParams.totalWidth + 'mm" height="' + jobParams.totalHeight + 'mm" viewBox="0 0 ' + jobParams.totalWidth + ' ' + jobParams.totalHeight + '">\n';
   var polyCutText = '  <path ' + polyCutStyle + ' d="' + polysPathData + '"/>\n';
-  var frameCutText = '  <rect ' + frameCutStyle + ' x="' + frameCutX + '" y="' + frameCutX + '" width="' + frameCutWidth + '" height="' + frameCutHeight + '"/>\n';
+  var frameCutText = '  <rect ' + frameCutStyle + ' x="' + jobParams.frameCutX.toFixed(4) + '" y="' + jobParams.frameCutX.toFixed(4) + '" width="' + jobParams.frameCutWidth.toFixed(4) + '" height="' + jobParams.frameCutHeight.toFixed(4) + '"/>\n';
   if (forPreview) {
     exportText += frameCutText;
     exportText += polyCutText;
@@ -124,6 +91,29 @@ function polysToSVG(image, polys, forPreview) {
   exportText += '</svg>';
 
   return exportText;
+}
+
+function fillDerivedJobParams(sourceJobParams, image) {
+  var p = sourceJobParams; // alias
+
+  var imageMaxDim = Math.max(image.width, image.height);
+  var framePadPx = p.framePadFrac*imageMaxDim;
+  var withFrameWidthPx = image.width + 2*framePadPx;
+  var withFrameHeightPx = image.height + 2*framePadPx;
+
+  p.px2mm = Math.min(p.workAreaWidth/withFrameWidthPx, p.workAreaHeight/withFrameHeightPx);
+  p.insetTranslation = { // in mm
+    x: p.workAreaInset + framePadPx*p.px2mm,
+    y: p.workAreaInset + framePadPx*p.px2mm,
+  };
+
+  p.totalWidth = p.px2mm*withFrameWidthPx + 2*p.workAreaInset;
+  p.totalHeight = p.px2mm*withFrameHeightPx + 2*p.workAreaInset;
+
+  p.frameCutX = p.workAreaInset;
+  p.frameCutY = p.workAreaInset;
+  p.frameCutWidth = p.px2mm*withFrameWidthPx;
+  p.frameCutHeight = p.px2mm*withFrameHeightPx;
 }
 
 function setCurrentImage(image) {
@@ -143,12 +133,29 @@ function setCurrentImage(image) {
     var imageData = ctx.getImageData(0, 0, image.width, image.height);
     console.log('Extracted ' + imageData.data.length + ' bytes of image data');
 
+    // Derive further job params now that we have image dimensions
+    fillDerivedJobParams(testJobParams, image); 
+
     console.log('Sampling ...');
     var testStrips = new strips.SimpleVerticalStrips(image.width, image.height, 118, 200);
     currentPolys = testStrips.sampleToPolys(imageData, 1);
     console.log('Finished sampling');
     renderPolys(canvas, currentPolys);
 }
+
+// BEGIN JOB HARDCODE
+var testJobParams = {
+  framePadFrac: 0.05,
+  // We give an extra mm of safe area padding beyond what Ponoko says, so we do 6mm instead of 5mm
+  // These vars are in mm units
+  workAreaWidth: 788,
+  workAreaHeight: 382,
+  workAreaInset: 6,
+  beamWidth: 0.3, // Ponoko says 0.1-0.2mm on either side, so take average
+  laserCutColor: 'rgb(0,0,255)',
+  laserCutStrokeWidth: '0.01',
+};
+// END JOB HARDCODE
 
 document.addEventListener('DOMContentLoaded', function() {
   var dropTarget = document;
@@ -165,7 +172,7 @@ document.addEventListener('DOMContentLoaded', function() {
       return;
     }
 
-    var svgData = polysToSVG(currentImage, currentPolys, true);
+    var svgData = polysToSVG(currentImage, currentPolys, testJobParams, true);
 
     this.href = 'data:image/svg+xml,' + encodeURIComponent(svgData);
   }, false);
@@ -175,7 +182,7 @@ document.addEventListener('DOMContentLoaded', function() {
       return;
     }
 
-    var svgData = polysToSVG(currentImage, currentPolys, false);
+    var svgData = polysToSVG(currentImage, currentPolys, testJobParams, false);
 
     this.href = 'data:image/svg+xml,' + encodeURIComponent(svgData);
   }, false);
